904edd78233b0296fa38db33f10e76275415e5f4
Fizy1 <chineduedward@gmail.com>
2022-09-14 10:22:56 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,15 +3,13 @@
 /**
  * main - a function that prints the alphabet, in lowercase and a new line
  *
- * Description - a function that prints the alphabet, in lowercase
+ * Description - 'a function that prints the alphabet, in lowercase'
  *
  * Return: 0
  */
 
-	/* printing alphabet from a to z */
 
 	void print_alphabet(void)
-	/* printing alphabet */
 
 	{
 		char a;
